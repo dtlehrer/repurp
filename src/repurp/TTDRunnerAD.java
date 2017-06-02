99912a7400d81c7f918d24aebab26b0e802c8178
dtlehrer <dtlehrer@csbsju.edu>
2017-06-02 16:06:52 -0500

    relative file paths + script structure update

--- src/repurp/TTDRunnerAD.java
+++ src/repurp/TTDRunnerAD.java
@@ -7,14 +7,15 @@ public class TTDRunnerAD {
 
  public static void main(String args[]) throws IOException{
   Variables.originalDisease = "Alzheimer disease";
-  File file = new File("/net/home/dtlehrer/Thesis/TTDdata7.csv");
-  File proteinsfile = new File("/net/home/dtlehrer/edirect/ADproteinfile.out");
+  File file = new File("../../input/TTDdata7.csv");
+  File proteinsfile = new File("../../output/proteins/ADproteinfile.out");
+  System.out.println(proteinsfile.getAbsolutePath());
   //File genesfile = new File("/net/home/dtlehrer/edirect/genefile.out");
-  File symptomsfile = new File("/net/home/dtlehrer/Thesis/DiseaseSimilarities.csv");
+  File symptomsfile = new File("../../input/DiseaseSimilarities.csv");
   Scanner scanf = new Scanner(file);
   Scanner scanf2 = new Scanner(proteinsfile);
   Scanner scanf3 = new Scanner(symptomsfile);
-  File weightfile = new File("/net/home/dtlehrer/Desktop/Alzheimer disease.out");
+  File weightfile = new File("../../output/drugWeights/Alzheimer disease.out");
   PrintWriter weightOut = new PrintWriter(weightfile);
   Repurp rep = new Repurp(scanf, scanf2, scanf3, weightOut);
   rep.run();
